dc1849a1daf845f16a0b111d6d989a226f0c94bc
yuyujan <yu.main.address2@gmail.com>
2024-02-21 22:51:11 +0900

    なんかバグる

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+#実行されているOSに合わせて文字コードを選択
+#Windowsの場合はshift-jis
+#Linuxの場合はutf-8
+charset = "shift-jis" if os.name == "nt" else "utf-8"
+
 
 @client.event
 async def on_ready():
@@ -28,13 +33,22 @@ async def test_command(interaction: discord.Interaction , n: int = 4):
         n=10
         
     result = subprocess.run(["ping", "1.1.1.1", "-n", str(n)],
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="shift-jis")
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding=charset)
     if result.returncode == 0:
         await interaction.followup.send(result.stdout, ephemeral=False)
     else:
         await interaction.followup.send(result.stderr, ephemeral=False)
 
+@tree.command(name="pal_reboot" , description="パルワールドの再起動を行います。多用厳禁！")
+async def pal_reboot(interaction: discord.Interaction):
+    await interaction.response.defer(thinking=True)
+    result = subprocess.run(["sudo", "systemctl", "restart", "palworld-dedicated.service"],encoding=charset)
+    if result.returncode == 0:
+        await interaction.followup.send(result.stdout, ephemeral=False)
+    else:
+        await interaction.followup.send(result.stderr, ephemeral=False)
 # print(os.environ.get('TOKEN'))
+
 
 
 client.run(TOKEN)
