831e3149b3c014dec03420bbbb8ee8760f85030c
yuyujan <yu.main.address2@gmail.com>
2024-02-23 19:17:07 +0900

    更新

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-#実行されているOSに合わせて文字コードを選択
-#Windowsの場合はshift-jis
-#Linuxの場合はutf-8
+# 実行されているOSに合わせて文字コードを選択
+# Windowsの場合はshift-jis
+# Linuxの場合はutf-8
 charset = "shift-jis" if os.name == "nt" else "utf-8"
 
 
@@ -27,28 +27,51 @@ async def on_ready():
 
 
 @tree.command(name="ping_1111", description="pingを実行します。宛先は1.1.1.1です。")
-async def test_command(interaction: discord.Interaction , n: int = 4):
+async def test_command(interaction: discord.Interaction, n: int = 4):
     await interaction.response.defer(thinking=True)
-    if n>10:
-        n=10
-        
-    result = subprocess.run(["ping", "1.1.1.1", "-n", str(n)],
-                            stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding=charset)
-    if result.returncode == 0:
-        await interaction.followup.send(result.stdout, ephemeral=False)
-    else:
-        await interaction.followup.send(result.stderr, ephemeral=False)
+    if n > 10:
+        n = 10
+    try:
 
-@tree.command(name="pal_reboot" , description="パルワールドの再起動を行います。多用厳禁！")
+        if os.name == "nt":
+            result = subprocess.run(["ping", "1.1.1.1", "-n", str(n)],
+                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding=charset)
+        else:
+            result = subprocess.run(["ping", "1.1.1.1", "-c", str(n)],
+                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding=charset)
+        await interaction.followup.send(result.stdout, ephemeral=False)
+        return
+    except Exception as e:
+        await interaction.followup.send(str(e), ephemeral=False)
+        return
+
+
+@tree.command(name="pal_reboot", description="パルワールドの再起動を行います。多用厳禁！")
 async def pal_reboot(interaction: discord.Interaction):
     await interaction.response.defer(thinking=True)
-    result = subprocess.run(["sudo", "systemctl", "restart", "palworld-dedicated.service"],encoding=charset)
-    if result.returncode == 0:
-        await interaction.followup.send(result.stdout, ephemeral=False)
-    else:
-        await interaction.followup.send(result.stderr, ephemeral=False)
-# print(os.environ.get('TOKEN'))
+    try:
+        result = subprocess.run(
+            ["sudo", "systemctl", "restart", "palworld-dedicated.service"], encoding=charset)
+        
+        await interaction.followup.send("再起動コマンドを発行しました\n"+result.stdout, ephemeral=False)
 
+    except Exception as e:
+        await interaction.followup.send("パルワールドの再起動時にエラーが発生しました。", ephemeral=False)
+        return
+
+
+
+@tree.command(name="sudo_test", description="sudoできるかテストします。")
+async def sudo_test(interaction: discord.Interaction):
+    await interaction.response.defer(thinking=True)
+    try:
+        result = subprocess.run(["sudo", "whoami"], encoding=charset)
+
+        await interaction.followup.send(result.stdout, ephemeral=False)
+    except Exception as e:
+        await interaction.followup.send(str(e), ephemeral=False)
+        return
+# print(os.environ.get('TOKEN'))
 
 
 client.run(TOKEN)
